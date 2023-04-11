671ab48a780584ce839878a155d159aff862369e
Jeffrey <1115378579@qq.com>
2023-04-11 16:20:41 +0800

    feat: 回到顶部

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 window.onload = () => {
   useMobileMenu()
+  // 创建回到顶部按钮元素
+  back_to_top()
 }
 function useMobileMenu() {
   let mobile_menu = document.querySelector('.mobile_menu');
@@ -10,11 +12,41 @@ function useMobileMenu() {
     }
   })
   icon_caidan.addEventListener('click', (e) => {
-      changeMenu(true)
+    changeMenu(true)
   })
   function changeMenu(state) {
     mobile_menu.style.width = state ? '100vw' : '0';
     document.body.style.overflow = state ? 'hidden' : 'auto';
   }
   return [changeMenu]
+}
+function back_to_top() {
+  const scrollToTopBtn = document.createElement("div");
+  scrollToTopBtn.classList.add('iconfont', 'icon-backtop')
+
+  // 添加点击事件监听器
+  scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+
+  // 将按钮添加到页面中的 body 元素中
+  document.body.appendChild(scrollToTopBtn);
+
+  // 添加样式以将按钮放在右下角
+  scrollToTopBtn.style.position = "fixed";
+  scrollToTopBtn.style.bottom = "40px";
+  scrollToTopBtn.style.right = "40px";
+
+  // 监听窗口的滚动事件
+  window.addEventListener("scroll", function () {
+    // 如果滚动位置小于 200 像素，则隐藏回到顶部按钮，否则显示它
+    if (window.scrollY < 200) {
+      scrollToTopBtn.style.display = "none";
+    } else {
+      scrollToTopBtn.style.display = "block";
+    }
+  });
 }
