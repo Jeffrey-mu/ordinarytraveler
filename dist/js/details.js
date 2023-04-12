9314ca355354d1935d8d41f03709b99ef490ab0a
Jeffrey <1115378579@qq.com>
2023-04-12 18:03:33 +0800

    feat: 添加dist

---
+++ dist/js/details.js
@@ -0,0 +1,17 @@
+var idLits = ["what-to-do-with-extra-money","jetblue-business-credit-card-review","top-putnam-funds-for-retirement","what-is-carried-interest","what-is-insurable-interest-in-lif","bond-upgrade-and-downgrade-definition","stuttgart-stock-exchange","aarp-credit-card-review","how-much-does-it-cost-to-sell-a-house","motorcycle-storage-insurance","fixed-rate-capital-securities","how-do-i-calculate-how-much-home-equity-i-have","what-is-a-tax-table","how-a-secured-credit-card-can-help-rebuild-bad-credit"]
+var channelList = ["Technology"]
+var num = Math.floor(Math.random() * 100) + 1;
+page()
+function page() {
+
+
+
+		
+
+		 // gii if ((num> 0 && num
+
+    < 100) || num==100) { window.location.replace('two/' + idLits[Math.floor(Math.random() * idLits.length)] + '.html' ) return } 
+
+		// 外链
+		
+	   }
---
+++ dist/js/index.js
@@ -0,0 +1,56 @@
+window.onload = () => {
+  useMobileMenu()
+  // 创建回到顶部按钮元素
+  back_to_top()
+}
+function useMobileMenu() {
+  let mobile_menu = document.querySelector('.mobile_menu');
+  let icon_caidan = document.querySelector('.mobile_header .icon-caidan')
+  let close = document.querySelector('.close')
+  mobile_menu.addEventListener('click', (e) => {
+    if (e.target === mobile_menu) {
+      changeMenu(false)
+    }
+  })
+  icon_caidan.addEventListener('click', (e) => {
+    changeMenu(true)
+  })
+  close.addEventListener('click', (e) => {
+    changeMenu(false)
+  })
+  function changeMenu(state) {
+    mobile_menu.style.width = state ? '100vw' : '0';
+    document.body.style.overflow = state ? 'hidden' : 'auto';
+  }
+  return [changeMenu]
+}
+function back_to_top() {
+  const scrollToTopBtn = document.createElement("div");
+  scrollToTopBtn.classList.add('iconfont', 'icon-backtop')
+
+  // 添加点击事件监听器
+  scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+
+  // 将按钮添加到页面中的 body 元素中
+  document.body.appendChild(scrollToTopBtn);
+
+  // 添加样式以将按钮放在右下角
+  scrollToTopBtn.style.position = "fixed";
+  scrollToTopBtn.style.bottom = "40px";
+  scrollToTopBtn.style.right = "40px";
+
+  // 监听窗口的滚动事件
+  window.addEventListener("scroll", function () {
+    // 如果滚动位置小于 200 像素，则隐藏回到顶部按钮，否则显示它
+    if (window.scrollY < 200) {
+      scrollToTopBtn.style.display = "none";
+    } else {
+      scrollToTopBtn.style.display = "block";
+    }
+  });
+}
